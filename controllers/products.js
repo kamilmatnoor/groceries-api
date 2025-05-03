f1f0f7e9ac96e1b68d0a89067765bb6c487867d4
Muhamad Kamil Muhamad Noor <kamilmatnoor@gmail.com>
2025-05-03 19:11:08 +0800

    chore(product controllers): update method (get all, getById, create) to communicate with mongo db

--- controllers/products.js
+++ controllers/products.js
@@ -1,17 +1,23 @@
+const ProductModel = require("../models/product");
+
 const Product = (() => {
     const getAll = () =>
-        new Promise((resolve, reject) => {
-            resolve({ error: false, message: "Success: getAll Product, response from controller" });
+        new Promise(async (resolve, reject) => {
+            const product = await ProductModel.find();
+            resolve({ error: false, message: "Success: getAll Product, response from controller", data: product });
         });
 
     const getById = (id) =>
-        new Promise((resolve, reject) => {
-            resolve({ error: false, message: "Success: getById Product, response from controller" });
+        new Promise(async (resolve, reject) => {
+            const product = await ProductModel.findById(id);
+            resolve({ error: false, message: "Success: getById Product, response from controller", data: product });
         });
 
-    const create = (product) =>
-        new Promise((resolve, reject) => {
-            resolve({ error: false, message: "Success: create Product, response from controller" });
+    const create = (reqProduct) =>
+        new Promise(async (resolve, reject) => {
+            const { product_name, product_description, product_brand, product_barcode } = reqProduct;
+            const product = await ProductModel.create({ product_name, product_description, product_brand, product_barcode, });
+            resolve({ error: false, message: "Success: create Product, response from controller", data: product });
         });
 
 
